8b36f18b0162dfaf21e7e8650b9786230d650304
Justin Hunt <justin.hunt@georgiancollege.ca>
2019-01-21 15:57:26 -0500

    Initializer

---
+++ MusicStore/Models/MusicStoreContextInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.Models
+{
+    public class MusicStoreContextInitializer : DropCreateDatabaseAlways<MusicStoreContext>
+    {
+
+        protected override void Seed(MusicStoreContext context)
+        {
+            Genre metal = new Genre { Name = "Metal" };
+            context.Artists.Add(new Artist { Name = "System of a Down" });
+            context.Genres.Add(metal);
+            context.Albums.Add(new Album
+            {
+                Artist = new Artist { Name = "Scars on Broadway" },
+                Genre = metal,
+                Price = 9.99m,
+                Title = "Dictator"
+            });
+
+            base.Seed(context);
+        }
+
+    }
+}

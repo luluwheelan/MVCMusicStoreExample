426aedbc9be10d0b00c3aa1f957581c5ac7bfb90
Justin Hunt <justin.hunt@georgiancollege.ca>
2019-01-21 15:32:59 -0500

    inclass2

---
+++ MusicStore/Models/Artist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.Models
+{
+    public class Artist
+    {
+        public virtual int ArtistId { get; set; }
+        public virtual String Name { get; set; }
+
+    }
+}
---
+++ MusicStore/Models/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.Models
+{
+    public class Genre
+    {
+        public virtual int GenreId { get; set; }
+        public virtual String Name { get; set; }
+        public virtual String Description { get; set; }
+        public virtual List<Album> Albums { get; set; }
+    }
+}
